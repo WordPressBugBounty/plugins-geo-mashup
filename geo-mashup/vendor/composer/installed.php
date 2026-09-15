5fe4a1c16ac97ea3ea25ad6e30c2cc122e2ad6ae
Sean Murphy <sean@iamseanmurphy.com>
2026-09-15 04:48:44 +0000

    1.13.22

--- geo-mashup/vendor/composer/installed.php
+++ geo-mashup/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'cyberhobo/wordpress-geo-mashup',
-        'pretty_version' => '1.13.21',
-        'version' => '1.13.21.0',
-        'reference' => 'b13b27bbac840c0ab9bf43daed416fa0a734be04',
+        'pretty_version' => '1.13.22',
+        'version' => '1.13.22.0',
+        'reference' => '7a100a207052a5e7d7b550992c0309d612d5a7aa',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'cyberhobo/wordpress-geo-mashup' => array(
-            'pretty_version' => '1.13.21',
-            'version' => '1.13.21.0',
-            'reference' => 'b13b27bbac840c0ab9bf43daed416fa0a734be04',
+            'pretty_version' => '1.13.22',
+            'version' => '1.13.22.0',
+            'reference' => '7a100a207052a5e7d7b550992c0309d612d5a7aa',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
